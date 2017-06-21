66df51ba1ba9a39f3a90b53952c970667452daaa
Arrian Purcell <arrian.purcell@gmail.com>
2017-06-21 21:32:04 +1000

    Add extendRefs: true to keep old ref property merge behaviour (#1) Bumping ajv version Validate against FHIR 3.0.0 Generate schema json more accurately

--- test/test.js
+++ test/test.js
@@ -8,12 +8,15 @@ function log(item) {
 }
 
 // Sample Resources
-var validMedicationStatements = ['./examples/medicationstatementexample1.json',
-								'./examples/medicationstatementexample2.json',
-								'./examples/medicationstatementexample4.json',
-								'./examples/medicationstatementexample5.json',
-								'./examples/medicationstatementexample6.json',
-								'./examples/medicationstatementexample7.json'];
+var validMedicationStatements = [
+    './examples/medicationstatementexample1.json',
+	'./examples/medicationstatementexample2.json',
+    './examples/medicationstatementexample3.json',
+	'./examples/medicationstatementexample4.json',
+	'./examples/medicationstatementexample5.json',
+	'./examples/medicationstatementexample6.json',
+	'./examples/medicationstatementexample7.json'
+];
 
 
 describe('FHIR Validator', function() {
@@ -29,6 +32,17 @@ describe('FHIR Validator', function() {
     		log(result);
     		assert.equal(0, result.errors.length, result.errors);
     	});
+    });
+  });
+
+  describe('Patient validation', function() {
+    it('should not give any errors for valid patients', function () {
+        var patient = require('./examples/patient1.json'),
+            result;
+
+        result = fhir.validate(patient, 'Patient');
+        log(result);
+        assert.equal(0, result.errors.length, result.errors);
     });
   });
 
